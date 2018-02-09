0200ef127d39edf5e26c080a7b2e90379f1cd926
kharazzoro <kha.1991.14.2017@gmail.com>
2018-02-09 06:04:46 +0000

    create function to add categories and articles

--- server/db-client/connection.js
+++ server/db-client/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 
+ 
 var promise = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/aljarida');
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+ const dbClinetCate = require('../db-client/categories/category')
 /* GET home page. */
 router.get('/add-category', function(req, res, next) {
   res.render('add-category', { title: 'Express' });
@@ -12,6 +12,11 @@ router.get('/edit-category', function(req, res, next) {
 });
 
 router.post('/add-category', function(req, res, next) {
- });
+  let cb=()=>{
+      res.redirect('/')
+  }
+  dbClinetCate.saveCategory(req.body,cb);
+  // dbClinetCate.saveCategory
+});
 
 module.exports = router;
